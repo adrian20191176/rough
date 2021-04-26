9fba800261b359db1afcd4f59df4c17882e00d5f
unknown <adrian.20191176@iit.ac.lk>
2021-04-26 07:36:45 +0530

    Home

--- reachHigh-heb-app/src/components/AppDeveloper/How.js
+++ reachHigh-heb-app/src/components/AppDeveloper/How.js
@@ -8,9 +8,9 @@ function How(){
         <div class="resultContain">
                 <div class="subresultContain">
           <h2>In the attractiveness insights,if your app is</h2>
-          <ul>Free then you will be given score 01</ul>
-          <ul>Editor’s choice then you will be given 01</ul>
-        <ul>Beyond the range of downloads then you will be given score 01</ul>
+          <ul>Free then you will be given a score of 1</ul>
+          <ul>Editor’s choice then you will be given a score of 1</ul>
+        <ul>having above the limit of downloads then you will be given a score of 1</ul>
 
         </div>
         <div class="subresultContain">
@@ -20,10 +20,10 @@ function How(){
             <img src={left} width="100%" height="100%"/>
         </div>
         <div class="subresultContain">
-          <h2>In the trustworthiness insights, if your app is</h2>
+          <h2>In the trustworthiness insights, your app's</h2>
 
-          <ul>In good rating then you will be given 01</ul>
-          <ul>Filled with good reviews then you will be given 01</ul>
+          <ul>rating will be considered.</ul>
+          <ul>user reviews will be taken and each and every review gets a sentiment score. And Final Average of those reviews will be calculated to give score.</ul>
         </div>
       </div>
     )}
